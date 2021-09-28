e3735e68595c6954196def17dba41a42b322ed67
salmanahmad2955 <you@example.com>
2021-09-28 18:02:06 +0200

    Basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
+import React, { Component } from 'react'
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>My React app</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
