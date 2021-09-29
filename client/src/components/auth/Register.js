90c6fb7e5e0d77b674f3b14fc8ca1700910b1360
salmanahmad2955 <you@example.com>
2021-09-29 19:55:36 +0200

    reg and login done

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
 
 class Register extends Component {
@@ -30,8 +32,10 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    console.log(newUser);
-  }
+    axios.post('/api/users/register', newUser)
+    .then(res => console.log(res.data))
+    .catch(err => this.setState({ errors: err.response.data}));
+  };
 
   render() {
     const { errors } = this.state;
@@ -45,11 +49,13 @@ class Register extends Component {
               <p className="lead text-center">
                 Create your DevHub account
               </p>
-              <form noValidate onSubmit={this.onSubmit}>
+              {/* noValidate is used to prevent front end errors */}.
+              <form noValidate onSubmit={this.onSubmit}> 
                 <div className="form-group">
                   <input
                     type="text"
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', 
+                    {'is-invalid': errors.name})}
                     placeholder="Name"
                     name="name"
                     value={this.state.name}
@@ -62,7 +68,8 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="email"
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', 
+                    {'is-invalid': errors.email})}
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
@@ -79,7 +86,8 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="password"
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', 
+                    {'is-invalid': errors.password})}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
@@ -92,7 +100,8 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="password"
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', 
+                    {'is-invalid': errors.password2})}
                     placeholder="Confirm Password"
                     name="password2"
                     value={this.state.password2}
